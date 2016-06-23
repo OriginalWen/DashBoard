dd8a9a7c601c9669c6c74c8cc0abbf3f42fcfa3f
yuyuanyuan0807 <yuyuanyuan930807@gmail.com>
2016-06-23 10:17:11 -0400

    new update

--- app/scripts/controllers/work.js
+++ app/scripts/controllers/work.js
@@ -2,11 +2,10 @@
 // WORK CONTROLLER
 // Description: Define the following functionalities:
 // All the functionalities on the "Work" section to display items and filter them
-
 //var app = angular.module("myApp",[]);  
-    app.controller('work', function($scope,$http) {
+    app.controller('work', function($scope,$http, $filter) {
         
-      $scope.userName="aaa";
+     // $scope.userName="aaa";
         
         $http({
             
@@ -24,22 +23,32 @@
         console.log("error");
     });
     
-        
-        
+
+           
+        /*  $scope.greaterThan = function(prop, val){
+            
+            
+            return function(item){
+                
+            return item[prop] > val;
+            }
+            }
+        */
         $scope.deleteItem = function(item) {
         
-            var items = $scope.works;
-            var length = items.length;
+          //  var items = $scope.works;
+          //  var length = items.length;
             $scope.removeIndex = -1;
+            $scope.removeIndex=$scope.works.indexOf(item);
                // alert(item);
-            for (var i = 0; i < length; i++) {
+         /*   for (var i = 0; i < length; i++) {
                 if (angular.equals(items[i], item)) {
                     $scope.removeIndex = i;
                   //  alert($scope.removeIndex);
                     break;
                 }
                
-            }
+            }*/
         
         };
         
@@ -51,7 +60,7 @@
         
         
         $scope.addItems=function(){
-            alert($scope.addtitle);
+           // alert($scope.addtitle);
             var newItem={
                 
                 title: $scope.addtitle,
@@ -64,12 +73,46 @@
             }
            // alert($scope.addTitle);
             $scope.works.push(newItem);
+            $scope.addtitle="";
+            $scope.addAuthor="";
+            $scope.addLike="";
+            $scope.addComment="";
+            
+            
         }
         
-        /* $scope.editItems=function(item){
-             $scope.theEdit=item;
+         $scope.editItems=function(item){
+             $scope.editIndex = -1;
+            $scope.editIndex=$scope.works.indexOf(item);
+             $scope.edittitle=item.title;
+            $scope.editAuthor=item.author;
+            $scope.editLike=item.like;
+            $scope.editComment=item.comment;
              
-             
-         }*/
-   
+         }
+         
+         $scope.editConfirm = function() {
+             var i=$scope.editIndex;
+            $scope.works[i].author=$scope.editAuthor;
+            $scope.works[i].title=$scope.edittitle;
+             $scope.works[i].like=$scope.editLike;
+             $scope.works[i].comment=$scope.editComment;
+            
+        };
+      /*  $scope.greaterThan = function(input) {
+        return function(item) {                          
+            if ( parseInt(item['like'].substring(0,item['like'].length-7)) > parseInt(input)) {                
+                return true;
+            } else {                
+                return false;
+            }
+        }
+        };*/
+        
+        $scope.order = function(expression, reverse) {
+            $scope.works = $filter('orderBy')($scope.works, expression, reverse);
+        }
+
+        
     });
+
--- app/scripts/directives/dialog.js
+++ app/scripts/directives/dialog.js
@@ -2,35 +2,22 @@
 // DIALOG DIRECTIVE
 // Description: Define 2 directives in the app: addDialog and editDialog. 
 // First one is for the "Add" button and the second one is for the "Edit" button
-angular.module('myApp', [])
-    .directive('myDirective', function() {
-        return {
-                restrict: 'E',
-                template: '<a href="http://google.com">
-                    Click me to go to Google</a>'
-                }
-                });
-  /*  .directive("addDialog", function(){
-    return{
+
+
+
+app.directive('additemdirective',function(){
+	return{
+		restrict: 'E',
+        replace: true,
+		templateUrl: 'templates/addDialog.html'
         
-        restrict:'E',
-      //  templateUrl: 'templates/addDialog.html'
-        
-        template:"aaaaaa",
-        
-        
-    };
+        //template:'<p>aaa</p>'
+	};
 })
-    .directive("editDialog", function(){
-    return{
-        
-        restrict:"A",
-        templateUrl:'templates/editDialog.html'
-        
-        
-        
-    };
-        
-    
-    
-});*/
+.directive('edititemdirective',function(){
+	return{
+		restrict: 'E',
+        replace: true,
+		templateUrl: 'templates/editDialog.html'
+	};
+});
