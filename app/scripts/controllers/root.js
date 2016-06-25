3c3069a4c8d7f9daac56090988c5004d26ee0071
yuyuanyuan0807 <yuyuanyuan930807@gmail.com>
2016-06-25 16:30:18 -0400

    Update root.js

--- app/scripts/controllers/root.js
+++ app/scripts/controllers/root.js
@@ -4,18 +4,3 @@
 // Make service calls to retrieve user's informationto display on the header
 // Make service calls to log the user out of the app
 // Populate the current date that the user log in
-app.controller('root', function($scope, session, $state) {
-    
-     $scope.user = session.get('user');
-    $scope.date = new Date();
-    
-    
-
-    
-     $scope.logout = function() {
-       session.destroy("user");
-       session.destroy("password");
-       $state.go("login");       
-    }
-    
-});
