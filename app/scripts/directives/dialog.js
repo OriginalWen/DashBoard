c0f37c79fc203c2feb40097bcfaa06fc712b6783
yuyuanyuan0807 <yuyuanyuan930807@gmail.com>
2016-06-25 16:29:32 -0400

    Update dialog.js

--- app/scripts/directives/dialog.js
+++ app/scripts/directives/dialog.js
@@ -2,22 +2,3 @@
 // DIALOG DIRECTIVE
 // Description: Define 2 directives in the app: addDialog and editDialog. 
 // First one is for the "Add" button and the second one is for the "Edit" button
-
-
-
-app.directive('additemdirective',function(){
-	return{
-		restrict: 'E',
-        replace: true,
-		templateUrl: 'templates/addDialog.html'
-        
-      
-	};
-})
-.directive('edititemdirective',function(){
-	return{
-		restrict: 'E',
-        replace: true,
-		templateUrl: 'templates/editDialog.html'
-	};
-});
